01bed5b85fd42a85e897ba31f63425fcac25b02d
Miloslav Staněk <445225@mail.muni.cz>
2018-03-16 16:35:21 +0100

    before you make mess of a layout

--- app/src/main/java/com/example/milos/chocolatefactory/TappingActivity.java
+++ app/src/main/java/com/example/milos/chocolatefactory/TappingActivity.java
@@ -5,11 +5,14 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class TappingActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private ImageButton mChocolateButton;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -37,6 +40,29 @@ public class TappingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_tapping);
 
         mTextMessage = (TextView) findViewById(R.id.message);
+        mChocolateButton = (ImageButton) findViewById(R.id.chocolateButton);
+//        mChocolateButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                String message = mMessageBodyEditText.getText().toString();
+//
+//                // in case user does not fill anything
+//                if (message.isEmpty()) {
+//                    Toast.makeText(getApplicationContext(), "Fill the message ;)", Toast.LENGTH_SHORT)
+//                            .show();
+//                    return;
+//                }
+//
+//                Intent intent = new Intent(getBaseContext(), SecondActivity.class);
+//                intent.putExtra("EXTRA_MESSAGE", message);
+//                startActivity(intent);
+//
+//                Toast.makeText(getApplicationContext(), "Sending " + message, Toast.LENGTH_SHORT)
+//                        .show();
+//            }
+//        });
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
