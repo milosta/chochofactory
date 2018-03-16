459c4deb574f5ba446ed887f1a6bb5379aafd230
Miloslav Staněk <445225@mail.muni.cz>
2018-03-16 15:38:22 +0100

    upgrade labels

--- app/src/main/java/com/example/milos/chocolatefactory/MainActivity.java
+++ app/src/main/java/com/example/milos/chocolatefactory/MainActivity.java
@@ -17,14 +17,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_tapping:
+                    mTextMessage.setText(R.string.title_tapping);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_building:
+                    mTextMessage.setText(R.string.title_building);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_upgrade:
+                    mTextMessage.setText(R.string.title_upgrade);
                     return true;
             }
             return false;
