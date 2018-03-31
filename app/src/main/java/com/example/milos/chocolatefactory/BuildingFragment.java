c21cd7daffae49d2691f70628e71aa29d9ccf552
Miloslav Staněk <milostanek@gmail.com>
2018-03-31 20:08:42 +0200

    fix first and add second frag

--- app/src/main/java/com/example/milos/chocolatefactory/BuildingFragment.java
+++ app/src/main/java/com/example/milos/chocolatefactory/BuildingFragment.java
@@ -1,5 +1,6 @@
 package com.example.milos.chocolatefactory;
 
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,22 +10,19 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link TappingFragment#newInstance} factory method to
+ * Use the {@link BuildingFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class TappingFragment extends Fragment {
-
-//    private TextView mTextMessage;
-//    private ImageButton mChocolateButton;
+public class BuildingFragment extends Fragment {
 
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @return A new instance of fragment TappingFragment.
+     * @return A new instance of fragment BuildingFragment.
      */
-    public static TappingFragment newInstance() {
-        TappingFragment fragment = new TappingFragment();
+    public static BuildingFragment newInstance() {
+        BuildingFragment fragment = new BuildingFragment();
         return fragment;
     }
 
@@ -32,7 +30,7 @@ public class TappingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tapping, container, false);
+        return inflater.inflate(R.layout.fragment_building, container, false);
     }
 
 }
