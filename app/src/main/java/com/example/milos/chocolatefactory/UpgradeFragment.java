e0ad50db17f4d021cb2adf776ae38e3b626565e6
Miloslav Staněk <milostanek@gmail.com>
2018-03-31 20:14:31 +0200

    last fragment

---
+++ app/src/main/java/com/example/milos/chocolatefactory/UpgradeFragment.java
@@ -0,0 +1,36 @@
+package com.example.milos.chocolatefactory;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link UpgradeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class UpgradeFragment extends Fragment {
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment UpgradeFragment.
+     */
+    public static UpgradeFragment newInstance() {
+        UpgradeFragment fragment = new UpgradeFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_upgrade, container, false);
+    }
+
+}
