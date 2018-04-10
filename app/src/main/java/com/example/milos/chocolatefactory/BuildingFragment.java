ef619538c3c0b36cc0bfac108f0291cffa56981d
Miloslav Staněk <445225@mail.muni.cz>
2018-04-10 19:01:57 +0200

    remove balast from buildign fraq

--- app/src/main/java/com/example/milos/chocolatefactory/BuildingFragment.java
+++ app/src/main/java/com/example/milos/chocolatefactory/BuildingFragment.java
@@ -23,36 +23,21 @@ import java.util.List;
  */
 public class BuildingFragment extends Fragment {
 
-    // TODO: Customize parameter argument names
     private static final String ARG_COLUMN_COUNT = "column-count";
-    // TODO: Customize parameters
     private int mColumnCount = 1;
-    private OnListFragmentInteractionListener mListener;
 
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
-    public BuildingFragment() {
-    }
+    private OnListFragmentInteractionListener mListener;
 
     // TODO: Customize parameter initialization
     @SuppressWarnings("unused")
-    public static BuildingFragment newInstance(int columnCount) {
+    public static BuildingFragment newInstance() {
         BuildingFragment fragment = new BuildingFragment();
-        Bundle args = new Bundle();
-        args.putInt(ARG_COLUMN_COUNT, columnCount);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if (getArguments() != null) {
-            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
-        }
     }
 
     @Override
